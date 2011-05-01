d59fd8bcfbab6f5d36acc3146c2caa7ed01f1a74
kurtextrem <kurtextrem@gmail.com>
2011-05-01 14:07:41 +0200

    Updated Language files and user/option/UserOptionOutputVideo.class.php

--- lib/data/user/option/UserOptionOutputVideo.class.php
+++ lib/data/user/option/UserOptionOutputVideo.class.php
@@ -28,57 +28,27 @@ class UserOptionOutputVideo implements UserOptionOutput {
 	 * @see UserOptionOutput::getOutput()
 	 */
 	public function getOutput(User $user, $optionData, $value) {
-		if ($value != "") {
+		$value = trim($value);
+
+		if (!empty($value)) {
 			StringUtil::encodeHTML($value);
-			$value = trim($value);
-			$slen = strlen($value);
-			$addi = '';
-			if (stristr($value, ";html5") === false) {
-				if ($slen <= 15) {
-					if (stristr($value, ";hd") !== false) {
-						$value = str_replace(';hd', '', $value);
-						$addi .= "&amp;hd=1";
-					}
-					if (stristr($value, ";color1") !== false || stristr($value, ";color2") !== false) {
-						preg_match_all('/;color[1-2]=[A-Za-z0-9]*/', $value, $match);
-						$addi .= "&amp".$match[0][0]."&amp".$match[0][1];
-						$value = preg_replace('/;color[1-2]=[A-Za-z0-9]*/', '', $value);
-					}
-					return '<div id="youtubevideo"><object type="application/x-shockwave-flash" style="width:425px; height:350px" data="http://www.youtube.com/v/'.$value.'?rel=0'.$addi.'"><param name="movie" value="http://www.youtube.com/v/'.$value.'?rel=0'.$addi.'" /></object></div>';
-				} else {
-					if (stristr($value, ";hd") !== false) {
-						$value = str_replace(';hd', '', $value);
-						$addi .= "&amp;hd=1";
-					}
-					if (stristr($value, ";color1") !== false || stristr($value, ";color2") !== false) {
-						preg_match_all('/;color[1-2]=[A-Za-z0-9]*/', $value, $match);
-						$addi .= "&amp".$match[0][0]."&amp".$match[0][1];
-						$value = preg_replace('/;color[1-2]=[A-Za-z0-9]*/', '', $value);
-					}
-					$value = str_replace('watch?v=', 'v/', $value);
-					return '<div id="youtubevideo"><object type="application/x-shockwave-flash" style="width:425px; height:350px" data="'.$value.'?rel=0'.$addi.'"><param name="movie" value="'.$value.'?rel=0'.$addi.'" /></object></div>';
-				}
-			} else {
-				// Dont kill me for that... this is the only "real" embed method 4 html5 videos from youtube. q.q
-				if ($slen <= 15) {
-					$value = str_replace(';html5', '', $value);
-					$value = str_replace(';hd', '', $value);
-					$value = preg_replace('/;color[1-2]=[A-Za-z0-9]*/', '', $value);
-					if (stristr($value, ";hd") !== false) {
-						$value = str_replace(';hd', '', $value);
-						$addi .= "&amp;hd=1";
-					}
-					return '<div id="youtubevideo"><iframe class="youtube-player" style="width:960px;height:750px;border:0px" src="http://www.youtube.com/embed/'.$value.'?rel=0'.$addi.'"></iframe></div>';
-				} else {
-					$value = str_replace('watch?v=', 'embed/', $value);
-					$value = str_replace(';html5', '', $value);
-					$value = preg_replace('/;color[1-2]=[A-Za-z0-9]*/', '', $value);
-					if (stristr($value, ";hd") !== false) {
-						$value = str_replace(';hd', '', $value);
-						$addi .= "&amp;hd=1";
-					}
-					return '<div id="youtubevideo"><iframe class="youtube-player" style="width: 425px;height: 350px;border:0px" src="'.$value.'?rel=0'.$addi.'"></iframe></div>';
-				}
+			$parsed_url = parse_url($value);
+
+			switch ($parsed_url['host']) {
+				case 'youtube.com':
+					break;
+
+				case 'vimeo.com':
+					break;
+
+				case 'clipfish.de':
+					break;
+
+				case 'myvideo.de':
+					break;
+
+				default:
+					return '<div id="color: red">Error! Unknown URL</div>';
 			}
 		}
 	}
--- readme.md
+++ readme.md
@@ -20,16 +20,16 @@ Youtube und Vimeo.
 Youtube Styling
 ---------------
 Seit Version 1.4 kann man Flash Videos designen und sogar HTML5 "erzwingen". Einfach bei dem Eingabefeld folgendes hinten ran hängen:
- * ";html5" = HTML5 wird "erzwungen"
+ * `;html5` = HTML5 wird "erzwungen"
 
- * ";hd" = Die Videos starten automatisch in HD.
+ * `;hd` = Die Videos starten automatisch in HD.
 
- * ";color1=\*" & ";color2=\*" = Farbe vom Player festlegen. Funktioniert **nur** mit **Flash**.
+ * `;color1=*" & ";color2=\*` = Farbe vom Player festlegen. Funktioniert **nur** mit **Flash**.
 
- * Beispiel: "http://www.youtube.com/watch?v=362OUej4t7k;hd;color1=0xe1600f;color2=0xfebd01" = HD Flash Video mit Gelb / Rotem Player.
+ * Beispiel: `http://www.youtube.com/watch?v=362OUej4t7k;hd;color1=0xe1600f;color2=0xfebd01` = HD Flash Video mit Gelb / Rotem Player.
 
 **Viel spaß damit, Kurtextrem**
 
-Video at the Profil (English)
+Video on the Profil (English)
 =============================
 Coming soon!
