cbecda82345b09a085704bcfa20e3e9ba8636db9
kurtextrem <kurtextrem@gmail.com>
2011-05-02 16:40:04 +0200

    added www. to reduce bytes xD

--- lib/data/user/option/UserOptionOutputVideo.class.php
+++ lib/data/user/option/UserOptionOutputVideo.class.php
@@ -35,29 +35,25 @@ class UserOptionOutputVideo implements UserOptionOutput {
 			$parsed_url = parse_url($value);
 			$return = '<div id="video">';
 
-			if(empty($parsed_url['query']))
+			if (empty($parsed_url['query']))
 				$parsed_url['query'] = '';
-			if (!isset($parsed_url['host'])){
-				$host = '';
-			}else{
-				$host = str_replace('www.', '', $parsed_url['host']);
-			}
+			$host = 'www.'.str_replace('www.', '', $parsed_url['host']); // i do it so, cause sometime there is a www. url so i need to replace it first.
 
 			preg_match_all('/;([^;]+)/', $parsed_url['path'].$parsed_url['query'], $matches);
 			$replace = array('hd' => 'hd=1', 'autoplay' => 'autplay=1', 'loop' => 'loop=1');
 
 			switch ($host) {
-				case 'youtube.com':
+				case 'www.youtube.com':
 					preg_match('/v=([^&#;]+)/', $parsed_url['query'], $matches);
 					$return .= '<object type="application/x-shockwave-flash" style="width:425px; height:349px" data="//www.youtube.com/v/'.$matches.'?'.$flags.'"><param name="movie" value="//www.youtube.com/v/'.$matches.'?'.$flags.'" /><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param></object>';
 					break;
 
-				case 'vimeo.com':
+				case 'www.vimeo.com':
 					$path = str_replace('/', '', $parsed_url['path']);
 					$return .= '<object width="400" height="225"><param name="allowfullscreen" value="true" /><param name="allowscriptaccess" value="always" /><param name="movie" value="http://vimeo.com/moogaloop.swf?clip_id='.$path.$flags.'" /></object>';
 					break;
 
-				case 'myvideo.de':
+				case 'www.myvideo.de':
 					preg_match('~/watch/(.*)/.*~', $parsed_url['path'], $matches);
 					$return .= '<object style="width:425px;height:350px;" width="425" height="350"><param name="movie" value="http://www.myvideo.de/movie/'.$value.'"></param><param name="AllowFullscreen" value="true"></param><param name="AllowScriptAccess" value="always"></param><embed src="http://www.myvideo.de/movie/'.$value.'" width="425" height="350" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true"></embed></object>';
 					break;
